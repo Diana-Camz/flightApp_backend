ae0f7b9758bd743124497151ab4a81f97d785d68
Diana Campos <dcampos0495@gmail.com>
2024-09-06 19:56:41 -0600

    create Home Screen

---
+++ client/src/screens/Flights.js
@@ -0,0 +1,112 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import Loader from '../components/Loader'
+import FlightCard from '../components/FlightCard';
+
+
+export default function Flights({navigation}) {
+  const [user, setUser] = useState([])
+  const [flights, setFlights] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if(user){
+      fetchData();
+      setLoading(false)
+    }
+
+  }, [user])
+
+  async function fetchData() {
+    try {
+      const response = await fetch('http://192.168.1.82:8080/flights/2');
+      const data = await response.json();
+      setFlights(data)
+      setLoading(false);
+      //console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  if (loading) {
+    return (
+      <Loader height={850}/>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header_container}>
+        <Text style={styles.title}>My Flights</Text>
+        <View style={styles.name_container}>
+          {/* <Text style={styles.name_user}>{user.name} {user.lastname}</Text> */}
+          {/* <Pressable onPress={signOutWithFirebase}> */}
+          <Pressable>
+            <Text style={styles.logout}>log out</Text>
+          </Pressable>
+        </View>
+      </View>
+      {flights == 0
+        ? <Text style={styles.title_empty}>You haven't booked any flights yet</Text>
+        : <FlatList
+        data={flights}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => 
+          <Pressable onPress={() => navigation.navigate('Update', {id: item.id})}>
+            <FlightCard {... item}/>
+          </Pressable>}
+      /> }
+      <Ionicons name={'add-circle'} size={90} style={styles.iconAdd} onPress={()=>{navigation.navigate('Origin')}}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 15,
+    paddingTop: 50,
+    marginBottom: 60,
+  },
+  header_container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  name_container: {
+    paddingRight: 5,
+  },
+  name_user: {
+    fontSize: 17,
+    color: '#725099',
+    fontWeight: '500',
+  },
+  logout: {
+    textAlign: 'center',
+    paddingTop: 3,
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#3441bd',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: '700',
+    color: '#9700FF',
+  },
+  title_empty: {
+    height: 400,
+    fontSize: 30,
+    fontWeight: '500',
+    color: '#a18ab4',
+    textAlign: 'center',
+    paddingTop: 250,
+  },
+  iconAdd: {
+    position: 'absolute',
+    color: '#9700FF',
+    alignSelf: 'center',
+    marginTop: 670,
+  }
+});
