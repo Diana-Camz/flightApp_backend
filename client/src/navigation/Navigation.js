7ccdad9011ea52bdb8da6d30e92245422d1aa914
Diana Campos <dcampos0495@gmail.com>
2024-09-09 21:37:08 -0600

    change navigation for new screens

--- client/src/navigation/Navigation.js
+++ client/src/navigation/Navigation.js
@@ -2,11 +2,11 @@ import { StyleSheet } from 'react-native'
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import Flights from '../screens/Flights'
-// import Origin from '../screens/bookingScreens/Origin'
-// import Destiny from '../screens/bookingScreens/Destiny'
-// import Dates from '../screens/bookingScreens/Dates'
-// import Passengers from '../screens/bookingScreens/Passengers'
-// import Confirm from '../screens/bookingScreens/Confirm'
+import Origin from '../screens/bookingScreens/Origin'
+import Destiny from '../screens/bookingScreens/Destiny'
+import Dates from '../screens/bookingScreens/Dates'
+import Passengers from '../screens/bookingScreens/Passengers'
+import Confirm from '../screens/bookingScreens/Confirm'
 import Update from '../screens/updateScreens/Update'
 // import OriginUpdate from '../screens/updateScreens/OriginUpdate'
 // import DestinyUpdate from '../screens/updateScreens/DestinyUpdate'
@@ -25,11 +25,11 @@ const Navigation = () => {
       {/* <Stack.Screen name="Login" component={Login}/>
       <Stack.Screen name="CreateAccount" component={CreateAccount}/> */}
 
-      {/* <Stack.Screen name="Origin" component={Origin}/>
+      <Stack.Screen name="Origin" component={Origin}/>
       <Stack.Screen name="Destiny" component={Destiny}/>
       <Stack.Screen name="Dates" component={Dates}/>
       <Stack.Screen name="Passengers" component={Passengers}/>
-      <Stack.Screen name="Confirm" component={Confirm}/> */}
+      <Stack.Screen name="Confirm" component={Confirm}/>
 
       <Stack.Screen name="Update" component={Update}/>
       
