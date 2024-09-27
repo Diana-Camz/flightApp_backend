fe0bba375fdc55ff664206933b84873d88aa96d4
Diana Campos <dcampos0495@gmail.com>
2024-09-27 17:50:00 -0600

    update frontend functions and resolve problems to get, post and put data successfully

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,6 +3,7 @@ import {BASE_URL} from '@env';   // 'http://10.0.2.2:' default way to connect to
 const API_FLIGHTS = `${BASE_URL}/flights`;
 const API_FLIGHT = `${BASE_URL}/flight`;
 const API_USER = `${BASE_URL}/user`
+const API_LOGIN = `${BASE_URL}/login`
 
 // GET
 export const getFlights = async (user_id) => {
@@ -15,19 +16,24 @@ export const getFlight = async (id) => {
     return await res.json()
 }
 
-// export const getUserById = async (user_id) => {
-//     const res = await fetch(`${API_USER}/${user_id}`)
-//     return await res.json()
-// }
+export const getUserById = async (user_id) => {
+    const res = await fetch(`${API_USER}/${user_id}`)
+    return await res.json()
+}
 
 // POST - CREATE
 export const saveFlight = async (newFlight) => {
-    const res = await fetch(API_FLIGHTS, {
+    const user_id = newFlight.user_id;
+   try {
+    const res = await fetch(`${API_FLIGHTS}/${user_id}`, {
         method: 'POST',
         headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
         body: JSON.stringify(newFlight)
     });
     return await res.json()
+   } catch (error) {
+     console.error('Error saving flight in api.js', error)
+   }
 }
 
 export const createUser = async (newUser) => {
@@ -37,29 +43,43 @@ export const createUser = async (newUser) => {
             headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
             body: JSON.stringify(newUser) 
         });
-        if (!res.ok) {
-            throw new Error('Network response was not ok');
-        }
+        return res.json()
+    } catch (error) {
+        console.error('Error creating user in api.js', error);
+    }
+}
+
+export const loginUser = async (userData) => {
+    try {
+        const res = await fetch(API_LOGIN, {
+            method: 'POST',
+            headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
+            body: JSON.stringify(userData) 
+        });
         return await res.json()
     } catch (error) {
-        console.error('Error creating user:', error);
-        throw error
+        console.error('Error login in api.js', error)
     }
 }
 
 // PUT - UPDATE
 export const updateFlight = async (id, value) => {
-    const res = await fetch(`${API_FLIGHT}/${id}`, {
-        method: 'PUT',
-        headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
-        body: JSON.stringify(value)
-    })
-    return res.json();
+    try {
+        const res = await fetch(`${API_FLIGHT}/${id}`, {
+            method: 'PUT',
+            headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
+            body: JSON.stringify(value)
+        })
+        return await res.json()
+    } catch (error) {
+        console.error('Error updating flight in api.js', error)
+    }
 }
 
-//DELETE
+// DELETE
 export const deleteFlight = async (id) => {
-    await fetch(`${API_FLIGHT}/${id}`, {
+    const res = await fetch(`${API_FLIGHT}/${id}`, {
         method: 'DELETE',
-    })
+    });
+    return await res.json()
 }
