ebf2b278dc7d14c7203826b76f06fcaf72977e8b
Diana Campos <dcampos0495@gmail.com>
2024-09-26 18:06:38 -0600

    get data from flights and user according to user that login

--- client/src/screens/Flights.js
+++ client/src/screens/Flights.js
@@ -4,14 +4,16 @@ import { Ionicons } from '@expo/vector-icons';
 import Loader from '../components/Loader'
 import FlightCard from '../components/FlightCard';
 import { useFlights } from '../hooks/useFlights';
+import { useUser } from '../hooks/useUser';
 
 
-export default function Flights({navigation}) {
-  const [user, setUser] = useState([])
-  const {flights, loading} = useFlights(2)
+export default function Flights({navigation, route}) {
+  const {user_id} = route.params;
+  const {user, loadingUser} = useUser(user_id)
+  const {flights, loading} = useFlights(user_id)
 
 
-  if (loading) {
+  if (loading || loadingUser) {
     return (
       <Loader height={850}/>
     );
@@ -22,7 +24,7 @@ export default function Flights({navigation}) {
       <View style={styles.header_container}>
         <Text style={styles.title}>My Flights</Text>
         <View style={styles.name_container}>
-          {/* <Text style={styles.name_user}>{user.name} {user.lastname}</Text> */}
+          <Text style={styles.name_user}>{user.name} {user.lastname}</Text>
           {/* <Pressable onPress={signOutWithFirebase}> */}
           <Pressable>
             <Text style={styles.logout}>log out</Text>
@@ -35,11 +37,11 @@ export default function Flights({navigation}) {
         data={flights}
         keyExtractor={item => item.id}
         renderItem={({item}) => 
-          <Pressable onPress={() => navigation.navigate('Update', {id: item.id})}>
+          <Pressable onPress={() => navigation.navigate('Update', {id: item.id, user_id: user_id})}>
             <FlightCard {... item}/>
           </Pressable>}
       /> }
-      <Ionicons name={'add-circle'} size={90} style={styles.iconAdd} onPress={()=>{navigation.navigate('Origin')}}/>
+      <Ionicons name={'add-circle'} size={90} style={styles.iconAdd} onPress={()=>{navigation.navigate('Origin', {user_id: user_id})}}/>
     </View>
   );
 }
